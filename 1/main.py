4917b00fd98f907c3b787c7d8415098423c86bdf
Iuri Guilherme <2027447+iuriguilherme@users.noreply.github.com>
2022-12-18 23:25:17 -0300

    days 1 2

---
+++ 1/main.py
@@ -0,0 +1,16 @@
+"""https://adventofcode.com/2022/day/1"""
+
+input_file: str = "input.txt"
+
+with open(input_file, 'r') as input_:
+    calories: list[tuple] = [(n, sum([int(food) for food in \
+        elf.split('\n') if food not in ['']])) for n, elf in \
+        enumerate(input_.read().split('\n\n'))
+    ]
+
+elfs, calories = zip(*calories)
+top: int = max(calories)
+
+print(f"""The elf carrying most calories is elf \
+#{elfs[calories.index(top)]} which is \
+carrying {top} calories""")
